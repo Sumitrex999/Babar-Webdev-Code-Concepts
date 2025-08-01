9fffe610b9829418ba4df0055b143b7d20118ebe
Sumit Rex <sumitrex666@gmail.com>
2025-08-01 18:30:26 +0530

    Initial commit - Learn Closure

---
+++ CodehelpYTWebDev HTML CSS JS/LearnClosure/index.js
@@ -0,0 +1,12 @@
+function outerFunction() {
+  let name = "Babbar";
+
+  function innerFunction() {
+    console.log(name);
+  }
+
+  return innerFunction;
+}
+let inner = outerFunction();
+
+inner();
